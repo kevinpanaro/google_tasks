bc2bbaa6549d9c6769194e0e05247693f258cfa2
kevinpanaro <panaro.kevin@gmail.com>
2018-08-19 15:25:59 -0600

    google_tasks: update init and core init commit

---
+++ google_tasks/__init__.py
@@ -0,0 +1,4 @@
+from .core import core
+
+from .task_parser import TaskParser, UnexpectedDayName
+from .wrapper import GoogleTasksWrapper, NoSuchTaskList
---
+++ google_tasks/core.py
@@ -0,0 +1,17 @@
+try:
+    from wrapper import GoogleTasksWrapper
+    from task_parser import TaskParser
+except ModuleNotFoundError:
+    from .wrapper import GoogleTasksWrapper
+    from .task_parser import TaskParser
+
+def core():
+    google_tasks = GoogleTasksWrapper()
+    tasks = TaskParser()()
+
+    for task in tasks:
+        task_list, task = task
+        google_tasks.add(task_list, task)
+
+if __name__ == '__main__':
+    core()
