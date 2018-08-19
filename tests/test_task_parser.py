ff5c69b2ff419edf5d493b7a6de19ee64024fabc
kevinpanaro <panaro.kevin@gmail.com>
2018-08-19 15:24:46 -0600

    google_tasks: tests: add task_parser test

---
+++ tests/test_task_parser.py
@@ -0,0 +1,24 @@
+try:
+    from context import google_tasks
+except ModuleNotFoundError:
+    from .context import google_tasks
+
+import unittest
+
+
+class TestTaskParser(unittest.TestCase):
+
+    def setUp(self):
+        self.good_task = "tasks_good.json"
+        self.bad_task = "tasks_bad.json"
+        self.TP = google_tasks.task_parser.TaskParser
+
+    def test_good_task(self):
+        self.assertIsInstance(self.TP(self.good_task)(), list)
+
+    def test_bad_task(self):
+        with self.assertRaises(google_tasks.task_parser.UnexpectedDayName):
+            self.TP(self.bad_task)
+
+if __name__ == '__main__':
+    unittest.main()
